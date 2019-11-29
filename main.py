d22e3c5c6064d5ccaf16403577e60754d6c2857f
Vadim Korotov <mpcomp12@gmail.com>
2019-11-29 15:32:26 +0300

    Added question rotation and shufle

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from quizzle import getQuestions, Question
 from tkinter import *
+from random import shuffle
+from time import sleep
 
-class QuestionBlock(object):
-    def __init__(self, master, q:Question):
-        #Текст вопроса
-        self.question = q
-        self.label = Label(master, text = q.question,font = "Jokerman 20")
+class QuestionBlock(Frame):
+
+
+    def create_widgets(self):
+        q = self.question
+        self.label = Label(self, text = q.question,font = "Jokerman 20")
         #Генерируем кнопки с ответами в произвольном порядке
         self.buttons = []
+
         for key in q.answers:
-            btn = Button(root ,text = q.answers[key],
+            btn = Button(self ,text = q.answers[key],
                                font = "Jokerman 20",
                                background = "#1E90FF",
                                foreground = "#FFFFFF",
@@ -19,13 +23,36 @@ class QuestionBlock(object):
             self.buttons.append(btn)
         # пакуем в окно
         self.label.pack()
+        shuffle(self.buttons)
         for btn in self.buttons:
             btn.pack()
+
     def checkAnswer(self, key, btn):
+        self.label_answer = Label(self, text ='', font = ("Arial", 24, 'bold') )
         if key == self.question.right:
-            btn['background'] = "#0f9d58"
+            btn['bg'] = "#0F9D58"
+            btn.flash()
+            self.label_answer.config(text= "Правильно!")
+            self.label_answer['fg'] = "#0F9D58"
         else:
-            btn['background'] = "#e50914"
+            btn['background'] = "#E50914"
+            btn.flash()
+            self.label_answer['text'] = "Не верно"
+            self.label_answer['fg'] = "#E50914"
+        self.label_answer.pack()
+        sleep(2)
+        self.quit()
+    def __init__(self, master, q:Question):
+        Frame.__init__(self, master)
+        self.pack()
+        #Текст вопроса
+        self.question = q
+        # Создаем элементы
+        self.create_widgets()
+
+
+
+
 
 root = Tk("Викторина")
 root.title("Викторина")
@@ -33,9 +60,11 @@ root.geometry("400x320+800+200")
 
 # Загружаем вопросы
 questions = getQuestions()
-fq = questions[0]
 
 # инициируем наш класс заготовку с вопросом
-QuestionBlock(root,fq)
+for currq in questions:
+    app = QuestionBlock(root,currq)
+    app.mainloop()
+    app.destroy()
 
-root.mainloop()
+root.destroy()
